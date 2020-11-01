7d04ab8188847efc50db6600854274d246ed2b33
Hassan <hassanalihazaraa@gmail.com>
2020-11-01 23:55:39 +0100

    added image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent-bru2-1.cdninstagram.com/v/t51.2885-19/s320x320/38194989_303893960417449_3206698221862649856_n.jpg?_nc_ht=scontent-bru2-1.cdninstagram.com&_nc_ohc=pK-uZMmCTwIAX8wv1K8&oh=a6f362fbc51e9624e56181b145a2c4be&oe=5FC7C111" class="rounded-circle">
+        </div>
+        <div class="col-9" pt-5>
+            <div><h1>Hassan</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>46</strong>posts</div>
+                <div class="pr-4"><strong>281</strong>followers</div>
+                <div class="pr-4"><strong>226</strong>followings</div>
             </div>
         </div>
     </div>
