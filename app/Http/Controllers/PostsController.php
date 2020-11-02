77ec85fcb62a8e41ceadd6a090de4ed3dd5493db
Hassan <hassanalihazaraa@gmail.com>
2020-11-02 22:32:37 +0100

    resize image to 1200px and added comments

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 
+use \Intervention\Image\Facades\Image;
+
 class PostsController extends Controller
 {
     public function __construct()
     {
+        //make this entire class authenticated
         $this->middleware('auth');
     }
 
@@ -17,17 +20,25 @@ class PostsController extends Controller
 
     public function store()
     {
+        //validate the the caption and image
         $data = request()->validate([
             'caption' => 'required',
             'image' => ['required', 'image']
         ]);
 
+        //store image to the path public storage upload folder
         $imagePath = request('image')->store('uploads', 'public');
 
+        //resize the image by using intervention/image
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+        $image->save();
+
+        //if use is logged in then store the caption and image
         auth()->user()->posts()->create([
             'caption' => $data['caption'],
             'image' => $imagePath
         ]);
+        //redirect user to profile page if he is logged in
         return redirect('/profile/' . auth()->user()->id);
     }
 }
