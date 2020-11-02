df6f3bbac5e1223cf305002811185ae14c057415
Hassan <hassanalihazaraa@gmail.com>
2020-11-02 22:55:36 +0100

    added caption and username

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,18 @@
 
 @section('content')
     <div class="container">
-        <img src="/storage/{{$posts->image}}" alt="">
+        <div class="row">
+            <div class="col-8">
+                <img src="/storage/{{$posts->image}}" alt="" class="w-100">
+            </div>
+
+            <div class="col-4">
+                <div>
+                    <h3>{{$posts->user->username}}</h3>
+                    <p>{{$posts->caption }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
