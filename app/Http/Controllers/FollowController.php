4472c22da7b3b960279a4d071f44de28f43631b2
Hassan <hassanalihazaraa@gmail.com>
2020-11-03 15:04:25 +0100

    users can follow and unfollow

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -8,6 +8,7 @@ class FollowController extends Controller
 {
     public function store(User $user)
     {
-        return $user->username;
+        //authenticated user can connect to the profile
+        return auth()->user()->following()->toggle($user->profile);
     }
 }
