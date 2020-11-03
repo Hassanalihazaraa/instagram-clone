9580af0b46fbe9ec6151d654ee641f5cdceb2015
Hassan <hassanalihazaraa@gmail.com>
2020-11-03 13:08:47 +0100

    added prodile picture beside username and description of the post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,20 @@
 
             <div class="col-4">
                 <div>
-                    <h3>{{$posts->user->username}}</h3>
+                    <div class="d-flex align-items-center">
+                        <div class="pr-3">
+                            <img src="/storage/{{$posts->user->profile->image}}" class="rounded-circle w-100" alt=""
+                                 style="max-width: 40px;">
+                        </div>
+                        <div>
+                            <div class="font-weight-bold">
+                                <a href="/profile/{{$posts->user->id}}">{{$posts->user->username}}</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr>
+
                     <p>{{$posts->caption }}</p>
                 </div>
             </div>
